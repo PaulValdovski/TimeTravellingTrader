1c1e2f14a6d3f7b46aa9e4732433610a59814242
PaulValdovski <149264637+PaulValdovski@users.noreply.github.com>
2023-11-02 00:11:21 +0200

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,8 +4,7 @@
 
 <div class="container">
     <h1>About us</h1>
-    <p>This web application was made for a university project. It allows the user to see which stock performed the best in a given time interval.</p>
-    <p>It was created using the Laravel framework. To make it run, you will have to install Laravel and import the DATABASE FILE.sql as the database "ttt".</p>
+    <p>This web application was made for a university project. It allows the user to see which stock performed the best in a given time interval. It was created using the Laravel framework.</p>
     <p>The current algorithm for finding the best performing stock can be improved. Also the data from the database is limited to the S&P500 top 100 companies. Despite all that, it is still fun to use, at least for a few minutes. Enjoy!</p>
 </div>            
 
